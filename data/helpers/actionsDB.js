1a5d2a40577d64ea192778ddefb7b78270528bbb
AnthonyJCampbell <anthony@anthonyjcampbell.com>
2019-03-29 10:11:13 +0100

    Added Actions helpers

--- data/helpers/actionsDB.js
+++ data/helpers/actionsDB.js
@@ -1,5 +1,20 @@
 const db = require('./../dbConfig');
 
 module.exports = {
+  insertAction,
+  getActions,
+  getAction
+}
 
+function insertAction(action) {
+  return db('actions')
+    .insert(action)
+}
+
+function getActions() {
+  return db('actions')
+}
+
+function getAction(id) {
+  return db('actions').where('action_id', '=', id)
 }
