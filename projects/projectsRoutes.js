05e89533ad6acc42955c7526ba47c7dfbd53f06f
AnthonyJCampbell <anthony@anthonyjcampbell.com>
2019-03-29 10:18:36 +0100

    Added GET, GETBYID, and POST routes for Projects

--- projects/projectsRoutes.js
+++ projects/projectsRoutes.js
@@ -4,4 +4,43 @@ const projectsDB = require('./../data/helpers/projectsDB')
 const router = express.Router();
 router.use(express.json());
 
+router.get('/', (req, res) => {
+  projectsDB.getProjects()
+    .then(data => {
+      res.status(200).json(data)
+    })
+    .catch(err => {
+      res.status(500).json(err)
+    })
+})
+
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  projectsDB.getProject(id)
+    .then(data => {
+      res.status(200).json(data)
+    })
+    .catch(err => {
+      res.status(500).json(err)
+    })
+})
+
+// Currently returns id of newly-made project
+router.post('/', (req, res) => {
+  const { project_name, project_desc } = req.body
+  if (!project_name || !project_desc) {
+    res.status(404).json({
+      message: "Make sure you pass a project_name and project_desc!"
+    })
+  } else {
+    projectsDB.insertProject(req.body)
+      .then(data => {
+        res.status(200).json(data)
+      })
+      .catch(err => {
+        res.status(500).json(err)
+      })
+  }
+})
+
 module.exports = router;
