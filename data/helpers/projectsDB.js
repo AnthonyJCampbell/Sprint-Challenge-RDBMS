882499cef41865d57cd78c89194836ab6740de5a
AnthonyJCampbell <anthony@anthonyjcampbell.com>
2019-03-29 10:09:24 +0100

    Added project helpers

--- data/helpers/projectsDB.js
+++ data/helpers/projectsDB.js
@@ -1,5 +1,20 @@
 const db = require('./../dbConfig');
 
 module.exports = {
-  
+  insertProject,
+  getProjects,
+  getProject
+}
+
+function insertProject(project) {
+  return db('projects')
+    .insert(project)
+}
+
+function getProjects() {
+  return db('projects')
+}
+
+function getProject(id) {
+  return db('projects').where('project_id', '=', id)
 }
